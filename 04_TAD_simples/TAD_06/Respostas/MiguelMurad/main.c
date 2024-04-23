37cb7280af4adff4aa93b581b6ae372e46d5f59f
MiguelZMurad <miguel.murad@edu.ufes.br>
2024-04-23 18:17:22 -0300

    TAD_06

---
+++ 04_TAD_simples/TAD_06/Respostas/MiguelMurad/main.c
@@ -0,0 +1,95 @@
+#include "matrix_utils.h"
+#include <stdio.h>
+
+int main () {
+    tMatrix matrix1, matrix2, result;
+    int operacao = 0;;
+    int rows1, cols1, rows2, cols2;
+
+    scanf("%d %d", &rows1, &cols1);
+    matrix1 = MatrixCreate(rows1, cols1);
+    matrix1 = MatrixRead(matrix1);
+
+    scanf("%d %d", &rows2, &cols2);
+    matrix2 = MatrixCreate(rows2, cols2);
+    matrix2 = MatrixRead(matrix2);
+
+
+    while (operacao != 6) {
+       
+        printf("1 - Somar matrizes\n2 - Subtrair matrizes\n3 - Multiplicar matrizes\n4 - Multiplicacao de uma matriz por escalar\n5 - Transposta de uma matriz\n6 - Encerrar o programa\nOpcao escolhida: \n");
+
+        scanf("%d", &operacao);
+
+        if (operacao == 1) {
+            if (PossibleMatrixSum(matrix1, matrix2) == 0) {
+                printf("Erro: as dimensoes da matriz nao correspondem\n\n");
+            }
+            else {
+
+                result = MatrixAdd(matrix1, matrix2);
+
+                MatrixPrint(result);
+            }
+        }
+       
+        if (operacao == 2) {
+           
+            if (PossibleMatrixSub(matrix1, matrix2) == 0) {
+                printf("Erro: as dimensoes da matriz nao correspondem\n\n");
+            }
+            else {
+
+                result = MatrixSub(matrix1, matrix2);
+
+                MatrixPrint(result);
+            }
+        }
+
+        if (operacao == 3) {
+            if (PossibleMatrixMultiply(matrix1, matrix2) == 0) {
+                printf("Erro: o numero de colunas da primeira matriz eh diferente do numero de linhas da segunda matriz\n\n");
+            }
+            else {
+
+                result = MatrixMultiply(matrix1, matrix2);
+
+                MatrixPrint(result);
+            }
+        }
+
+        if (operacao == 4) {
+            int qual, scalar;
+
+            scanf("%d", &scalar);
+            scanf("%d", &qual);
+
+            if (qual == 1) {
+           
+                matrix1 = MatrixMultiplyByScalar(matrix1, scalar);
+           
+                MatrixPrint(matrix1);
+            }
+            else {          
+                matrix2 = MatrixMultiplyByScalar(matrix2, scalar);
+           
+                MatrixPrint(matrix2);
+            }
+        }
+
+        if (operacao == 5) {
+           
+            result = TransposeMatrix(matrix1);
+           
+            MatrixPrint(result);
+
+            printf("\n");
+           
+            result = TransposeMatrix(matrix2);
+           
+            MatrixPrint(result);        
+        }
+    }
+
+    return 0;
+}
---
+++ 04_TAD_simples/TAD_06/Respostas/MiguelMurad/matrix_utils.c
@@ -0,0 +1,159 @@
+#include "matrix_utils.h"
+#include <stdio.h>
+
+
+tMatrix MatrixCreate(int rows, int cols) {
+    tMatrix matrix;
+
+    matrix.cols = cols;
+
+    matrix.rows = rows;
+
+    return matrix;
+}
+
+tMatrix MatrixRead(tMatrix matrix) {
+    int i, j;
+
+    for (i = 0; i < matrix.rows; i++) {
+        for (j = 0; j < matrix.cols; j++) {
+            scanf("%d", &matrix.data[i][j]);
+        }
+    }
+
+    return matrix;
+}
+
+
+void MatrixPrint(tMatrix matrix) {
+    int i, j;
+
+    for (i = 0; i < matrix.rows; i++) {
+        printf("|");
+        for (j = 0; j < matrix.cols; j++) {
+            printf("%d", matrix.data[i][j]);
+            if (j != matrix.cols-1) {
+                printf(" ");
+            }
+            else {
+                printf("|");
+                printf("\n");
+            }
+        }
+    }
+    printf("\n");
+}
+
+
+int PossibleMatrixSum(tMatrix matrix1, tMatrix matrix2) {
+    
+    if (matrix1.rows == matrix2.rows && matrix1.cols == matrix2.cols) {
+        return 1;
+    }
+
+    return 0;
+}
+
+
+int PossibleMatrixSub(tMatrix matrix1, tMatrix matrix2) {
+    
+    if (matrix1.rows == matrix2.rows && matrix1.cols == matrix2.cols) {
+        return 1;
+    }
+
+    return 0;
+}
+
+
+int PossibleMatrixMultiply(tMatrix matrix1, tMatrix matrix2) {
+    
+    if (matrix1.cols == matrix2.rows) {
+        return 1;
+    }
+
+    return 0;
+}
+
+
+tMatrix MatrixAdd(tMatrix matrix1, tMatrix matrix2) {
+    int i, j;
+    tMatrix result;
+
+    result.rows = matrix1.rows;
+    result.cols = matrix1.cols;
+
+    for (i = 0; i < matrix1.rows; i++) {
+        for (j = 0; j < matrix1.cols; j++) {
+            result.data[i][j] = matrix1.data[i][j] + matrix2.data[i][j];
+        }
+    }
+
+    return result;
+}
+
+
+tMatrix MatrixSub(tMatrix matrix1, tMatrix matrix2) {
+    int i, j;
+    tMatrix result;
+
+    result.rows = matrix1.rows;
+    result.cols = matrix1.cols;
+
+    for (i = 0; i < matrix1.rows; i++) {
+        for (j = 0; j < matrix1.cols; j++) {
+            result.data[i][j] = matrix1.data[i][j] - matrix2.data[i][j];
+        }
+    }
+
+    return result;
+}
+
+
+tMatrix MatrixMultiply(tMatrix matrix1, tMatrix matrix2) {
+    int i, j, k;
+    tMatrix result;
+
+    result.rows = matrix1.rows;
+    result.cols = matrix2.cols;
+
+    for (i = 0; i < matrix1.rows; i++) {
+        for (j = 0; j < matrix2.cols; j++) {
+            result.data[i][j] = 0;
+            for (k = 0; k < matrix1.cols; k++) {
+                result.data[i][j] += matrix1.data[i][k] * matrix2.data[k][j];
+            }
+        }
+    }
+
+    return result;
+}
+
+
+tMatrix TransposeMatrix(tMatrix matrix) {
+    int i, j;
+    tMatrix result;
+
+    result.rows = matrix.cols;
+    result.cols = matrix.rows;
+
+    for (i = 0; i < matrix.cols; i++) {
+        for (j = 0; j < matrix.rows; j++) {
+            result.data[i][j] = matrix.data[j][i];
+        }
+    }
+
+    return result;
+}
+
+
+tMatrix MatrixMultiplyByScalar(tMatrix matrix, int scalar) {
+    int i, j;
+
+    for (i = 0; i < matrix.rows; i++) {
+        for (j = 0; j < matrix.cols; j++) {
+            matrix.data[i][j] = matrix.data[i][j] * scalar;
+        }
+    }
+
+    return matrix;
+}
